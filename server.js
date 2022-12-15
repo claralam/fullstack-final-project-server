88ac7d7d52dbb396af6132c5afb0319182088f07
Clara Lam <claralam@andrew.cmu.edu>
2022-12-14 23:57:31 -0500

    Server side for get, post, put, and delete api endpoints complete

--- server.js
+++ server.js
@@ -1,76 +1,96 @@
 const express = require('express');
 const cors = require('cors');
-// const multer = require('multer')
+// const multer = require('multer');
 const fs = require('fs');
-// const memeLib = require("./memeGenerator.js");
 // const upload = multer({ dest: './images/' })
 
 const app = express();
 const port = 3005;
 
 let recipes = [];
-let currentRecipe = '{}';
-let stagedRecipeData = '';
 
 
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
 app.use(express.json());
-// The images directory that is served as a static server
+app.use(express.urlencoded({ extended: false }));
 // app.use('/images', express.static('images'));
 
 // Gets all recipes
-app.get('/recipes', (req, res) => {
+app.get('/api/recipes', (req, res) => {
     res.send(recipes);
 });
 
-// Posts a new recipe to the book
-app.post('/upload/', (req, res) => {
-    currentRecipe = req.body;
-    const findRecipe = recipes.find(f => f.recipeName.toLowerCase() === currentRecipe.recipeName.toLowerCase());
+app.get('/api/recipes/:id', (req, res) => {
+    const recipeName = req.params.id;
+    const findRecipe = recipes.find(f => f.recipeName === recipeName);
     if (findRecipe) {
-        res.send('This recipe already exists.')
+        const recipe = recipes.find(r => r.recipeName === recipeName);
+        res.send(recipe)
+    } else {
+        res.sendStatus(403);
+    }
+})
+
+// Posts a new recipe to the book
+app.post('/api/upload', (req, res) => {
+    currentRecipe = req.body;
+
+    const findRecipe = recipes.find(f => f.recipeName === currentRecipe.recipeName);
+    if (findRecipe) {
+        res.send('A recipe with this name already exists.')
     } else {
         recipes.push(currentRecipe)
         fs.writeFileSync('./data/recipes.json', JSON.stringify(recipes));
         res.send(`Adding recipe to book: ${currentRecipe.recipeName}`)
-        console.log(recipes)
     }
 });
 
 // Edits a recipe
-app.put('/edit', (req, res) => {
-    currentRecipe = req.body;
-    const findRecipe = recipes.find(f => f.recipeName.toLowerCase() === currentRecipe.recipeName.toLowerCase());
-    if (findRecipe) {
-        recipes = recipes.filter((recipe) => { return recipe.recipeName.toLowerCase() !== currentRecipe.recipeName.toLowerCase() });
-        recipes.push(currentRecipe);
-        console.log(recipes)
-        fs.writeFileSync('./data/recipes.json', JSON.stringify(recipes));
-        console.log(recipes)
-        res.send('Editing recipe: ' + currentRecipe.recipeName);
+app.put('/api/edit/:id', (req, res) => {
+    let editRecipeName = req.params.id;
+    let currentRecipe = req.body;
+
+    const findRecipe = recipes.find(f => f.recipeName === editRecipeName);
+    const findNewNameRecipe = recipes.find(f => f.recipeName === currentRecipe.recipeName);
+    if (findNewNameRecipe) {
+        res.send("There is already a recipe with this name.");
     } else {
-        res.send("Recipe does not exist.");
+        if (findRecipe) {
+            recipes = recipes.filter((recipe) => { return recipe.recipeName !== editRecipeName });
+            recipes.push(currentRecipe);
+            fs.writeFileSync('./data/recipes.json', JSON.stringify(recipes));
+            console.log(recipes)
+            res.send('Editing recipe: ' + editRecipeName);
+        } else {
+            res.send("Recipe does not exist.", res.status);
+        }
     }
 });
 
 // Deletes a recipe
-app.delete('/delete', (req, res) => {
-    const removeRecipe = req.body;
-    if (removeRecipe) {
-        recipes = recipes.filter((recipe) => { return recipe.recipeName.toLowerCase() !== removeRecipe.recipeName.toLowerCase() });
+app.delete('/api/delete/:id', (req, res) => {
+    console.log("PARAMS: ", req.params)
+    let removeRecipeName = req.params.id;
+    if (removeRecipeName) {
+        recipes = recipes.filter((recipe) => { return recipe.recipeName !== removeRecipeName });
         fs.writeFileSync('./data/recipes.json', JSON.stringify(recipes));
-        res.send('Removed recipe: ' + removeRecipe.recipeName);
-        console.log(recipes);
+        res.send('Removed recipe: ' + removeRecipeName);
+    } else{
+        res.send("Error: Cannot delete")
     }
+    console.log("ALL RECIPES: ", recipes)
 });
 
 app.listen(port, () => {
+    // If the data folder does not exist, create it
     if (!fs.existsSync('./data/')) {
         fs.mkdirSync('./data');
         console.log('Created data directory');
     }
+    
+    // Create recipes.json file
     const recipesFile = './data/recipes.json';
     if (fs.existsSync(recipesFile)) {
         const rawData = fs.readFileSync(recipesFile);
@@ -79,17 +99,11 @@ app.listen(port, () => {
         // File doesn't exist so create it
         fs.writeFileSync('./data/recipes.json', JSON.stringify(recipes));
     }
+
+    // Logging information so we know it works
     console.log(recipes)
     console.log("length: ", recipes.length)
     console.log(`Loaded ${recipes.length} recipes!`);
     
-    const currentFile = './data/currentRecipe.json';
-    if (fs.existsSync(currentFile)) {
-        const rawData = fs.readFileSync('./data/currentRecipe.json');
-        currentRecipe = JSON.parse(rawData);
-    } else {
-        fs.writeFileSync('./data/currentMeme.json', currentRecipe);
-    }
-    console.log('Loaded currentRecipe!', currentRecipe);
     console.log('Recipe API listening on port ' + port);
 });
